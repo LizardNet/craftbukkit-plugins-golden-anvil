c05c5acd5fa8089052a568743cb8faa90c4fea09
Andrew "FastLizard4" Adams <FastLizard4-Git-Commits@fastlizard4.org>
2013-12-07 15:57:54 -0800

    Define golden anvil ItemStacks, add/hook crafting recipes
    *Defines ItemStacks for Golden Anvils, Slightly Damaged Golden Anvils, and Very
     Damaged Golden Anvils
    *Defines crafting recipe for Golden Anvils
    *Hooks crafting recipe into the server in onEnable
    *Adds removal of crafting recipe in onDisable
    
    Change-Id: I3b4c662616ae981e650ea64673ef8007124f6bd7

--- src/main/java/org/fastlizard4/git/craftbukkit_plugins/golden_anvil/GoldenAnvils.java
+++ src/main/java/org/fastlizard4/git/craftbukkit_plugins/golden_anvil/GoldenAnvils.java
@@ -23,17 +23,77 @@
 
 package org.fastlizard4.git.craftbukkit_plugins.golden_anvil;
 
+import java.util.Iterator;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Recipe;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class GoldenAnvils extends JavaPlugin {
  
+	//We want the ItemStacks for the golden anvils to be available outside of just this class
+	public ItemStack goldenAnvil; //Material.ANVIL, damage value 3
+	public ItemStack goldenAnvilDamaged; //Material.ANVIL, damage value 4
+	public ItemStack goldenAnvilVeryDamaged; //Material.ANVIL, damage value 5
+	
     @Override
-    public void onEnable(){
-        // TODO Insert logic to be performed when the plugin is enabled
+    public void onEnable() {
+    	getLogger().info("Adding crafting recipe for golden anvils....");
+    	
+    	this.goldenAnvil = new ItemStack(Material.ANVIL, 1, (short)3);
+    	ItemMeta goldenAnvilMeta = this.goldenAnvil.getItemMeta();
+    	goldenAnvilMeta.setDisplayName("Golden Anvil");
+    	this.goldenAnvil.setItemMeta(goldenAnvilMeta);
+    	
+    	this.goldenAnvilDamaged = new ItemStack(Material.ANVIL, 1, (short)4);
+    	goldenAnvilMeta = this.goldenAnvilDamaged.getItemMeta();
+    	goldenAnvilMeta.setDisplayName("Slighly Damaged Golden Anvil");
+    	this.goldenAnvilDamaged.setItemMeta(goldenAnvilMeta);
+    	
+    	this.goldenAnvilVeryDamaged = new ItemStack(Material.ANVIL, 1, (short)5);
+    	goldenAnvilMeta = this.goldenAnvilVeryDamaged.getItemMeta();
+    	goldenAnvilMeta.setDisplayName("Very Damaged Golden Anvil");
+    	this.goldenAnvilVeryDamaged.setItemMeta(goldenAnvilMeta);
+    	
+    	ShapedRecipe goldenAnvilRecipe = new ShapedRecipe(this.goldenAnvil);
+    	goldenAnvilRecipe.shape("GGG", " g ", "ggg");
+    	goldenAnvilRecipe.setIngredient('G', Material.GOLD_BLOCK);
+    	goldenAnvilRecipe.setIngredient(' ', Material.AIR);
+    	goldenAnvilRecipe.setIngredient('g', Material.GOLD_INGOT);
+    	
+        if(getServer().addRecipe(goldenAnvilRecipe)) {
+        	getLogger().info("Successfully added golden anvil crafting recipe!");
+        } else {
+        	getLogger().severe("There is no crafting recipe, only Zuul!  (Adding the crafting recipe failed for some reason; I am now cowardly refusing to continue.)");
+        	getPluginLoader().disablePlugin(this);
+        }
     }
  
     @Override
     public void onDisable() {
-        // TODO Insert logic to be performed when the plugin is disabled
+    	getLogger().info("Attempting to unload golden anvils crafting recipe....");
+    	
+        Iterator<Recipe> recipeList = getServer().recipeIterator();
+        Recipe recipe;
+        try {
+        	while(recipeList.hasNext()) {
+        		recipe = recipeList.next();
+        		
+        		if(recipe.getResult() == this.goldenAnvil) {
+        			recipeList.remove();
+        			break;
+        		}
+        	}
+        } catch(Exception e) {
+        	getLogger().severe("Unable to unload golden anvils crafting recipe!  Expect undefined behavior!");
+        	getLogger().severe("The exception that occurred was: "+e.getMessage());
+        	getLogger().severe("A stack trace will now be printed to stderr.");
+        	e.printStackTrace();
+        }
+        
+        getLogger().info("Successfully unloaded golden anvils crafting recipe.  Bye!");
     }
 }
