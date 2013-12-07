14ebb2c7b08cd11460b794369d5855a74cd61bd6
Andrew "FastLizard4" Adams <FastLizard4-Git-Commits@fastlizard4.org>
2013-12-07 12:33:34 -0800

    Create Maven project.  I think.
    Change-Id: If0be8ff294958327bdb6577cf83dd3acb4b36215

---
+++ src/main/java/org/fastlizard4/git/craftbukkit_plugins/golden_anvil/App.java
@@ -0,0 +1,13 @@
+package org.fastlizard4.git.craftbukkit_plugins.golden_anvil;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
