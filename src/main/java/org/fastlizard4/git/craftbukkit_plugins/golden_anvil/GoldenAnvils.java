47adeadbe423e4db8060ee03ce3648d6e958bbaf
Andrew "FastLizard4" Adams <FastLizard4-Git-Commits@fastlizard4.org>
2013-12-07 13:37:21 -0800

    Establish Maven project and plugin skeleton
    *Adds plugin.yml
    *Modifies project name in pom.xml to match packaging
    
    Change-Id: I0b2f72fed2c75b3baadc6a1ca5b54d5e982c05a0

---
+++ src/main/java/org/fastlizard4/git/craftbukkit_plugins/golden_anvil/GoldenAnvils.java
@@ -0,0 +1,16 @@
+package org.fastlizard4.git.craftbukkit_plugins.golden_anvil;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GoldenAnvils extends JavaPlugin {
+ 
+    @Override
+    public void onEnable(){
+        // TODO Insert logic to be performed when the plugin is enabled
+    }
+ 
+    @Override
+    public void onDisable() {
+        // TODO Insert logic to be performed when the plugin is disabled
+    }
+}
